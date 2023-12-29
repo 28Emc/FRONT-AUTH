a4e29d806d04a6973ea32de9978f73f3a2da91bb
Edinson Medina Chinga <edmech25@gmail.com>
2023-12-29 17:03:18 -0500

    App.yaml updated and fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseURL: "",
-  secretKey: "",
-  titleSlim: "",
-  titleFull: ""
+  baseURL: "https://ms-auth-dot-lustrous-bonito-409316.rj.r.appspot.com/v1/api",
+  secretKey: "952f76ab50903522a6a74b5bc3507f12ed7df2ca3a1be0422f0de9788c1f6753",
+  titleSlim: "F-AUTH-GCP",
+  titleFull: "FRONT AUTH GCP"
 };
