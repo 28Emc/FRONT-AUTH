cf7783363997dc58f0e166e021a048523b7d41a4
Edinson Medina Chinga <edmech25@gmail.com>
2024-01-04 14:47:02 -0500

    Undo commit from v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseURL: "",
-  secretKey: "",
-  titleSlim: "",
-  titleFull: ""
+  baseURL: "https://ms-auth-dot-lustrous-bonito-409316.rj.r.appspot.com/v1/api",
+  secretKey: "5a3f8ef464f8f63a89c4c9fdb7082bd52d9e353730cf6eb76a8cb66f0ffbb46c",
+  titleSlim: "F-AUTH-GCP",
+  titleFull: "FRONT AUTH GCP"
 };
