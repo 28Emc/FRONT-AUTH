6bbb8b89b15ec0d697d6f7e6b37532379662a286
Edinson Medina Chinga <edmech25@gmail.com>
2024-01-04 14:19:31 -0500

    Updated api key prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseURL: "https://ms-auth-dot-lustrous-bonito-409316.rj.r.appspot.com/v1/api",
-  secretKey: "952f76ab50903522a6a74b5bc3507f12ed7df2ca3a1be0422f0de9788c1f6753",
+  secretKey: "5a3f8ef464f8f63a89c4c9fdb7082bd52d9e353730cf6eb76a8cb66f0ffbb46c",
   titleSlim: "F-AUTH-GCP",
   titleFull: "FRONT AUTH GCP"
 };
